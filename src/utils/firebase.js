fd522e912a19ae9a564458e5797adbf9bdf7be80
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2024-10-30 14:28:14 +0000

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,15 +1,15 @@
 // Import necessary Firebase modules
-import { initializeApp } from "firebase/app";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { 
+  getStorage, 
+  ref, 
+  uploadBytesResumable, 
+  getDownloadURL 
 } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4pX3SqmxE0zZUwbh0oGP0R01Vj46t1_A",
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
   authDomain: "blog-24dc3.firebaseapp.com",
   projectId: "blog-24dc3",
   storageBucket: "blog-24dc3.appspot.com",
@@ -18,9 +18,15 @@ const firebaseConfig = {
   measurementId: "G-SGPGBHWCCB",
 };
 
-// Initialize Firebase and Storage
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
+// Safe Firebase Initialization: 
+// Check if Firebase apps are already initialized to avoid re-initialization
+let app;
+if (typeof window !== "undefined") {
+  app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+}
 
-// Export app, storage, and Firebase storage utility functions
+// Initialize storage only if running in the browser
+const storage = app ? getStorage(app) : null;
+
+// Export Firebase utilities, checking for browser environment where necessary
 export { app, storage, ref, uploadBytesResumable, getDownloadURL };
