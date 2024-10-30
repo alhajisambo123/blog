d410a47ce21b88d1d80c6afff65a767ebd8f0a83
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2024-10-30 13:48:09 +0000

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,25 +4,25 @@ import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
-export const ThemeContextProvider = ({ children }) => {
-  const [theme, setTheme] = useState(null); // Start with 'null' to show it's loading
+const getFromLocalStorage = () => {
+  if (typeof window !== "undefined") {
+    const value = localStorage.getItem("theme");
+    return value || "light";
+  }
+};
 
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme") || "light";
-    setTheme(storedTheme);
-  }, []);
+export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    return getFromLocalStorage();
+  });
 
   const toggle = () => {
-    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   useEffect(() => {
-    if (theme) {
-      localStorage.setItem("theme", theme);
-    }
+    localStorage.setItem("theme", theme);
   }, [theme]);
-
-  if (theme === null) return null; // Prevent rendering until theme is loaded
 
   return (
     <ThemeContext.Provider value={{ theme, toggle }}>
