4f84516575bbcda625cdff8c56a8fae07a8e7779
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2024-11-02 09:54:58 +0000

    Update page.jsx

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 
 const getData = async (slug) => {
-  const res = await fetch(`https://blog-qd26.vercel.app/api/posts/${slug}`, {
+  const res = await fetch(`https://blog-one-blue-53.vercel.app/api/posts/${slug}`, {
     cache: "no-store",
   });
 
