bca6f4a813860b23379063c71c7469a9fdbbb134
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2024-11-05 21:47:21 +0000

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <Image src="/(1).jpeg" alt="facebook" width={24} height={24} />
+        <Image src="/(1).jpeg" alt="UGAHSA" width={60} height={60} />
         {/* <Image src="/instagram.png" alt="instagram" width={24} height={24} />
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} /> */}
@@ -20,9 +20,9 @@ const Navbar = () => {
         <Link href="/" className={styles.link}>
           Homepage
         </Link>
-        <Link href="/contact" className={styles.link}>
+        {/* <Link href="/contact" className={styles.link}>
           Contact
-        </Link>
+        </Link> */}
         <Link href="/about" className={styles.link}>
           About
         </Link>
