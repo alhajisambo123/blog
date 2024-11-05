c6bee55363ed36bdb55954c9427bcdbc2f00a57e
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2024-11-05 22:58:17 +0000

    Update MenuPosts.jsx

--- src/components/menuPosts/MenuPosts.jsx
+++ src/components/menuPosts/MenuPosts.jsx
@@ -41,6 +41,40 @@ const MenuPosts = ({ withImage }) => {
           </div>
         </div>
       </Link>
+       <Link href="/blog?cat=OT" className={styles.item}>
+        {withImage && (
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+        )}
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.culture}`}>OT</span>
+          <h3 className={styles.postTitle}>
+            Discover blogs tailored for Occupational Therapy professionals.
+          </h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Farouk</span>
+            <span className={styles.date}> - 10.03.2023</span>
+          </div>
+        </div>
+      </Link>
+       <Link href="/blog?cat=OT" className={styles.item}>
+        {withImage && (
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+        )}
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.culture}`}>OT</span>
+          <h3 className={styles.postTitle}>
+            Discover blogs tailored for Occupational Therapy professionals.
+          </h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Farouk</span>
+            <span className={styles.date}> - 10.03.2023</span>
+          </div>
+        </div>
+      </Link>
 
       <Link href="/blog?cat=Diet" className={styles.item}>
         {withImage && (
